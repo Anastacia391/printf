df6887f1f7c13db78e67d647e34cf79289198a09
Anastacia Warigi <anastaciawarigi@gmail.com>
2022-07-12 01:30:33 -0700

    _printf.c

---
+++ main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * check_for_specifiers - checks if there is a valid format specifier
+ * @format: possible format specifier
+ *
+ * Return: pointer to valid function or NULL
+ */
+static int (*check_for_specifiers(const char *format))(va_list)
+{
+		
